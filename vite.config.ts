1ef0fbf915f1bc6a3c5be3bf048a1747cec60a85
nyousfi-collab <nyousfi@charlotte.edu>
2025-11-19 22:02:05 -0500

    fix the base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bowtie-/',   // VERY IMPORTANT for GitHub Pages
+  base: '/nyousfi-collab/',   // VERY IMPORTANT for GitHub Pages
   plugins: [react()],
 })
